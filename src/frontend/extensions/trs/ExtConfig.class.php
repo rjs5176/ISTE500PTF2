7d6bf257754cf38ac6dabda7602d9c0fe5b77179
rjs5176 <rjs5176@rit.edu>
2020-02-01 16:25:49 -0500

    Created "trs" directories and ExtConfig.class.php
    Created "trs" directories for both the project front end and backend  and created the ExtConfig.class.php file in the front/src/extension/trs directory

---
+++ src/frontend/extensions/trs/ExtConfig.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: rjs5176
+ * Date: 02/01/2019
+ */
+
+
+namespace extensions\trs;
+
+
+class ExtConfig
+{
+    public const MENU = array(
+        'toyrequestsystem' => array(
+            'title' => 'Toy Request System',
+            'permission' => 'trs',
+            'icon' => '',
+            'link' => 'trs'
+        ),
+    );
+
+    public const ROUTES = array(
+        'trs' => '',
+    );
+}
